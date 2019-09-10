e2d36791b0a8c418c5114375b76300436e22136e
zwyu123 <zwyu2017@163.com>
2019-09-10 20:20:10 +0800

    访问日志过滤器

---
+++ coupon-gateway/src/main/java/com/imooc/coupon/filter/PreRequestFilter.java
@@ -0,0 +1,26 @@
+package com.imooc.coupon.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * <h1>在过滤器中存储客户端发起请求的时间戳</h1>
+ * Created by zwyu.
+ */
+@Slf4j
+@Component
+public class PreRequestFilter extends AbstractPreZuulFilter {
+
+    @Override
+    protected Object cRun() {
+
+        context.set("startTime", System.currentTimeMillis());
+
+        return success();
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+}
