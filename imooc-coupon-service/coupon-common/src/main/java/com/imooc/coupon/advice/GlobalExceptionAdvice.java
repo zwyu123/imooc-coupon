1949462f070a3436f4ce5dd5722ebcb1bb1c27d6
zwyu123 <zwyu2017@163.com>
2019-09-13 13:43:24 +0800

    统一异常处理

---
+++ imooc-coupon-service/coupon-common/src/main/java/com/imooc/coupon/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,33 @@
+package com.imooc.coupon.advice;
+
+import com.imooc.coupon.exception.CouponException;
+import com.imooc.coupon.vo.CommonResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <h1>全局异常处理</h1>
+ * Created by zwyu.
+ */
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    /**
+     * <h2>对 CouponException 进行统一处理</h2>
+     * @param req
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(value = CouponException.class)
+    public CommonResponse<String> handlerCouponException(
+            HttpServletRequest req, CouponException ex
+    ){
+
+        CommonResponse<String> response = new CommonResponse<>(
+                -1, "business error");
+        response.setData(ex.getMessage());
+        return response;
+    }
+}
