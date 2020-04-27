6d07180c23b454a208af30b167a2843bb36aff49
zwyu123 <zwyu2017@163.com>
2020-04-27 21:08:55 +0800

    Feign接口定义

---
+++ imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/feign/SettlementClient.java
@@ -0,0 +1,25 @@
+package com.imooc.coupon.feign;
+
+import com.imooc.coupon.exception.CouponException;
+import com.imooc.coupon.vo.CommonResponse;
+import com.imooc.coupon.vo.SettlementInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * <h1>优惠券结算微服务 Feign 接口定义</h1>
+ * Created by Qinyi.
+ */
+@FeignClient(value = "eureka-client-coupon-settlement")
+public interface SettlementClient {
+
+    /**
+     * <h2>优惠券规则计算</h2>
+     * */
+    @RequestMapping(value = "/coupon-settlement/settlement/compute",
+            method = RequestMethod.POST)
+    CommonResponse<SettlementInfo> computeRule(
+            @RequestBody SettlementInfo settlement) throws CouponException;
+}
