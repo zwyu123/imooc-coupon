3a95f4d8e648d18f28fdfe41196938b319cb6d0c
zwyu123 <zwyu2017@163.com>
2020-04-29 22:08:24 +0800

    优惠券模板规则处理器接口

---
+++ imooc-coupon-service/coupon-settlement/src/main/java/com/imooc/coupon/executor/RuleExecutor.java
@@ -0,0 +1,23 @@
+package com.imooc.coupon.executor;
+
+import com.imooc.coupon.constant.RuleFlag;
+import com.imooc.coupon.vo.SettlementInfo;
+
+/**
+ * <h1>优惠券模板规则处理器接口定义</h1>
+ */
+public interface RuleExecutor {
+
+    /**
+     * <h2>规则类型标记</h2>
+     * @return {@link RuleFlag}
+     * */
+    RuleFlag ruleConfig();
+
+    /**
+     * <h2>优惠券规则的计算</h2>
+     * @param settlement {@link SettlementInfo} 包含了选择的优惠券
+     * @return {@link SettlementInfo} 修正过的结算信息
+     * */
+    SettlementInfo computeRule(SettlementInfo settlement);
+}
